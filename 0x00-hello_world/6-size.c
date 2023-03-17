33d1be64bb8b96fb7d514c7f1e9e6f923946a245
fr13ndx <morsewallace254@gmail.com>
2023-03-17 14:51:43 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("size of an int is :%d", sizeof(int));
+	printf("size of an long int is :%d", sizeof(long int));
+	printf("size of an char is :%d", sizeof(char));
+	printf("size of an float is :%d", sizeof(float));
+}
