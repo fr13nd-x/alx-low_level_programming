d083ba7ba7fbec3ee64b8a08f324954a11b4ee60
fr13ndx <morsewallace254@gmail.com>
2023-03-17 15:34:27 -0700

    commit2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the phrase with proper grammar,but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 when exited 
  */
 int main(void)
 {
