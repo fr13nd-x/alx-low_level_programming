5d054a41d1c7aecf9ae6d7a27bb1d3b6cffa792d
fr13ndx <morsewallace254@gmail.com>
2023-03-17 15:24:35 -0700

    comm5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+*the program outputs the text Programming is like building a multilingual puzzle when the main function is called
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * the main function prints with proper grammar, but the outcome is a piece of art
+ */
+
 int main(void)
 { 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
