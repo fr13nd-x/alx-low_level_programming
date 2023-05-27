e46066084e61b8157b2198a46cc6ea83eacfd10c
fr13ndx <58890748+fr13nd-x@users.noreply.github.com>
2023-05-27 19:00:33 -0400

    Update 1-print_binary.c
    2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,22 +6,8 @@
  */
 void print_binary(unsigned long int n)
 {
-        unsigned long int temp;                                    int shifts;
+	if (n > 1)
+		print_binary(n >> 1);
 
-        if (n == 0)
-        {
-                printf("0");
-                return;
-        }
-
-        for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-                ;
-
-        for (; shifts >= 0; shifts--)
-        {
-                if ((n >> shifts) & 1)
-                        printf("1");
-                else
-                        printf("0");
-        }
+	_putchar((n & 1) + '0');
 }
